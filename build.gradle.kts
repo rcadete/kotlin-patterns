fbea4d0a0d61434250f7b5b8e428b04f0828aa47
kristalblue <kristalblue@ctu.gov>
2019-12-13 14:57:07 +0000

    Add log support

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,18 @@ repositories {
 }
 
 dependencies {
+    // kotlin
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Log4j
+    implementation("org.slf4j:slf4j-api:1.7.29")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("ch.qos.logback:logback-core:1.2.3")
+
+    // Test
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    testImplementation("org.mockito:mockito-core:3.2.0")
 }
 
